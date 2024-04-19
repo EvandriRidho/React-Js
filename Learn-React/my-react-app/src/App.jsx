ad863cf707f46437813b6dac085ebeb017446643
Evandri Ridho Hasmono <evandriridho321@gmail.com>
2024-04-19 17:42:50 +0700

    Overview UI-React

--- Learn-React/my-react-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App flex justify-center bg-slate-400 items-center">
-      <div>Hello World</div>
-    </div>
-  );
-}
-
-export default App;
---
+++ UI-React/src/App.jsx
@@ -0,0 +1,13 @@
+function Cup({ guest }) {
+  return <h2>Cangkir teh untuk tamu #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
